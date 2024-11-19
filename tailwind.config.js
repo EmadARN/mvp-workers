fd29804383d384c9afd01ff0bc0a3b72ec44404c
emad <arabanemad@gmail.com>
2024-11-19 17:01:31 +0330

    set config

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,10 +12,9 @@ function withOpacity(variableName) {
 
 module.exports = {
   content: [
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/common/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/container/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
