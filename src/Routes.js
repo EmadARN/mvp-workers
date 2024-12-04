b6b422e2c04f5ae23203f4836eb1a1e2e2658f1f
RezaFarzipour <rezafafa78@gmail.com>
2024-12-04 16:27:43 +0330

    dynamic routes added to Routes page

--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,10 @@
 import AboutUs from "./pages/aboutUs";
 import AllWorkerPage from "./pages/allWorkerPage";
+import SigninFinal from "./pages/auth/SigninFinal";
+import SigninImageForm from "./pages/auth/SigninImageForm";
+import SignInOtp from "./pages/auth/SignInOtp";
+import SignInPhoneNumber from "./pages/auth/SignInPhoneNumber";
+import SignInRegister from "./pages/auth/SignInRegister";
 import Home from "./pages/home";
 import ServicesUs from "./pages/servicesUs";
 import SignIn from "./pages/signIn";
@@ -17,8 +22,21 @@ const routes = [
     element: <AllWorkerPage />,
   },
   {
-    path: "/signIn/*",
+    path: "/signIn/",
     element: <SignIn />,
+    children: [
+      { path: "/SigninPhone", element: <SignInPhoneNumber /> },
+      { path: "/SigninOtp", element: <SignInOtp /> },
+      { path: "/SigninRegister", element: <SignInRegister /> },
+      {
+        path: "/SigninImage",
+        element: <SigninImageForm />,
+      },
+      {
+        path: "/SigninFinal",
+        element: <SigninFinal />,
+      },
+    ],
   },
 ];
 export default routes;
