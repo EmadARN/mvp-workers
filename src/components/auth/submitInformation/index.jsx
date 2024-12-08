0d577a2a17f081812b245eca3f57919cd7aa9ecb
emad <arabanemad@gmail.com>
2024-12-08 16:04:21 +0330

    update SignUpFinalPage

--- src/components/auth/submitInformation/index.jsx
+++ src/components/auth/submitInformation/index.jsx
@@ -1,36 +1,47 @@
 import { useNavigate } from "react-router-dom";
 import SubmitInputs from "./SubmitInputs";
 import Stepper from "../Stepper";
+import { useCookie } from "../../../hooks/useCookies";
+import { useAuthActions } from "../../../context/AuthReducer";
 
 const SignUpFinalPage = () => {
   const navigate = useNavigate();
+  const dispatch = useAuthActions();
+  const [cookieValue] = useCookie("auth-token");
 
-  const handleButtonClick = () => {
-    // Handling the final button click
-    navigate("/SignUpPage3");
+  const handleButtonClick = (isFinalized) => {
+    if (isFinalized) {
+      dispatch({
+        type: "FINALIZATION_SIGNUP_GET",
+        payload: { cookieValue },
+      });
+      navigate("/");
+    } else {
+      navigate("/signIn/SigninRegister");
+    }
   };
 
   return (
     <div className="mt-8 w-[100%]">
       <Stepper currentStep={4} />
 
-      <div className="container mx-auto text-center  w-full mb-5">
+      <div className="container mx-auto text-center w-full mb-5">
         <SubmitInputs />
         <p className="text-gray-600 text-lg font-bold mb-4">
           برای ثبت نهایی مشخصات خود را بررسی کرده و سپس بر روی دکمه ثبت اطلاعات
           حساب کلیک نمایید.
         </p>
       </div>
-      <div className="flex justify-center mb-8 ">
+      <div className="flex justify-center mb-8">
         <button
-          onClick={handleButtonClick}
-          className=" ml-5 transition duration-500 bg-main-1 text-gray-800 font-bold py-3 px-8 rounded-md hover:bg-gray-800 hover:text-main-1 hover:shadow-xl"
+          onClick={() => handleButtonClick(false)}
+          className="ml-5 transition duration-500 bg-main-1 text-gray-800 font-bold py-3 px-8 rounded-md hover:bg-gray-800 hover:text-main-1 hover:shadow-xl"
         >
           ویرایش
         </button>
 
         <button
-          onClick={handleButtonClick}
+          onClick={() => handleButtonClick(true)}
           className="transition duration-500 bg-main-1 text-gray-800 font-bold py-3 px-8 rounded-md hover:bg-gray-800 hover:text-main-1 hover:shadow-xl"
         >
           ثبت اطلاعات حساب
